a367d266bd55bb66608938a39a65fb75bc2e6c4a
RimmyAshika <160113752+RimmyAshika@users.noreply.github.com>
2025-05-05 21:50:40 +0530

    initial file added

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home() {
   return (
-   <div className='flex flex-row md:flex-col flex-wrap w-full h-120  '>
+   <div className='flex flex-row md:flex-col flex-wrap w-full h-auto  '>
         <div className=' bg-gradient-to-r to-[rgb(241,156,203)]   from-[rgb(39,0,32)]  md:w-200 md:rounded-e-full md:h-100 h-70 w-70 md:mx-20 mx-6 mt-20 md:mt-40 p-5 md:p-7 '>
           <p className='md:text-[30px] text-[#f5dbf5] md:mt-8 mt-10 text-[12px] font-serif '>Hello,It's Me </p>
           <h1 className='text-[#ccaeed] md:text-5xl text-[20px] font-semibold md:mt-2  font-serif '>Rimmy Ashika Verma</h1>
